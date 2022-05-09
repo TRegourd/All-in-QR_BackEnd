799ee7a807e2d2e0e358aa2c02fe05b353f706b9
alexis bleuse <bleuse.alexis@gmail.com>
2022-05-09 11:04:14 +0200

    route create attendees + models

---
+++ controllers/Attendees.js
@@ -0,0 +1,25 @@
+const AttendeesModel = require("../models/Attendees");
+
+const attendees = {
+  createAttendees(req, res) {
+    const attendeesForm = req.body;
+
+    console.log(attendeesForm);
+
+    if (!attendeesForm.name) return res.sendStatus(400);
+    if (!attendeesForm.surname) return res.sendStatus(400);
+    if (!attendeesForm.email) return res.sendStatus(400);
+    if (!attendeesForm.phone) return res.sendStatus(400);
+    if (!attendeesForm.extra_activities) return res.sendStatus(400);
+    if (!attendeesForm.event) return res.sendStatus(400);
+    if (!attendeesForm.role) return res.sendStatus(400);
+
+    AttendeesModel.create(attendeesForm)
+      .then(() => {
+        res.sendStatus(201);
+      })
+      .catch(() => res.sendStatus(500));
+  },
+};
+
+module.exports = attendees;
