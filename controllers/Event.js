620977dcf49f6ee0880a54e7b638af6c15046dc5
vinhphat <vinhphat@live.fr>
2022-05-10 10:02:13 +0200

    modified event controller

--- controllers/Event.js
+++ controllers/Event.js
@@ -3,7 +3,7 @@ const EvtModel = require("../models/Events");
 const events = {
   createEvent(req, res) {
     const { name, start_date, end_date, place, desc } = req.body;
-    console.log(start_date);
+
     if (!name) return res.sendStatus(400);
     if (!start_date) return res.sendStatus(400);
     if (!end_date) return res.sendStatus(400);
