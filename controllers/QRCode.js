c65d36602e89e0b4d852bac8c0aec6e58ab0f886
Jean-Baptiste Avella <jeanbaptiste.avella@gmail.com>
2022-05-12 16:39:49 +0200

    Added email sent to every attendee of an event

--- controllers/QRCode.js
+++ controllers/QRCode.js
@@ -1,6 +1,7 @@
 const AttendeesModel = require("../models/Attendees");
 var QRCodeNPM = require("qrcode");
 const QRCodeEmail = require("../libs/QRCodeEmail");
+const QRCodeEthe = require("../libs/QRCodeEmailEthereal");
 
 function generateQRCode(req, res, next) {
   QRCodeNPM.toDataURL(JSON.stringify(req.params.id), function (err, url) {
@@ -11,6 +12,21 @@ function generateQRCode(req, res, next) {
   res.send("QR Link Created");
 }
 
-const QRCode = { generateQRCode };
+function generateQRCodeAll(req, res, next) {
+  AttendeesModel.find({ event: req.params.id })
+    .then((data) => {
+      data.map((attendee) => {
+        QRCodeNPM.toDataURL(JSON.stringify(attendee._id), function (err, url) {
+          QRCodeEthe(attendee.email, url);
+        });
+      });
+      res.send("Emails envoyés");
+    })
+    .catch((error) => {
+      res.status(400).json({ error: error });
+    });
+}
+
+const QRCode = { generateQRCode, generateQRCodeAll };
 
 module.exports = QRCode;
