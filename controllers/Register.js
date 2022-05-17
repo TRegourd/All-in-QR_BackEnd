c8067c871cd8c5029b5942282f56f2057a245a6f
Thomas Regourd <tom.reg@hotmail.fr>
2022-05-17 08:34:09 +0200

    created stripe routes

--- controllers/Register.js
+++ controllers/Register.js
@@ -1,4 +1,7 @@
 const registerEmail = require("../libs/registerEmail");
+const { calculateOrderAmount } = require("../libs/calculateOrderAmount");
+const STRIPE_KEY = process.env.STRIPE_KEY;
+const stripe = require("stripe")(STRIPE_KEY);
 
 function emailRegisterPage(req, res) {
   const body = req.body;
@@ -9,6 +12,22 @@ function emailRegisterPage(req, res) {
     });
 }
 
-const Register = { emailRegisterPage };
+async function createPaymentIntent(req, res) {
+  const items = req.body;
+  console.log(calculateOrderAmount(items));
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "eur",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+}
+
+const Register = { emailRegisterPage, createPaymentIntent };
 
 module.exports = Register;
