3a674cf6ee9ea49acf30be3bb81ee94e6501bf67
Thomas Regourd <tom.reg@hotmail.fr>
2022-05-19 15:40:31 +0200

    updated contact route

--- controllers/Contact.js
+++ controllers/Contact.js
@@ -1,11 +1,16 @@
 const Contact = require("../models/Contacts");
 
 function postNewMessage(req, res) {
-  Contact.create(req.body)
-    .then(() => {
-      res.send(`New Message Created`);
-    })
-    .catch((error) => console.log(error));
+  console.log(req.body);
+  if (!req.body.message || !req.body.email) {
+    res.status(400).send("Incorrect input");
+  } else {
+    Contact.create(req.body)
+      .then(() => {
+        res.send(`New Message Created`);
+      })
+      .catch((error) => console.log(error));
+  }
 }
 
 const Contacts = { postNewMessage };
